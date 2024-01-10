3f9e97925745e51cddee441019fb2071f4bb10a2
Dee-Codez <debampati44@gmail.com>
2024-01-10 19:29:51 +0530

    Camera Permissions Done

---
+++ lib/controller/scan_controller.dart
@@ -0,0 +1,42 @@
+import 'package:camera/camera.dart';
+import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class ScanController extends GetxController {
+
+  @override
+  void onInit(){
+    super.onInit();
+    initCamera();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    cameraController.dispose();
+  }
+
+  late CameraController cameraController;
+  late List<CameraDescription> cameras;
+
+  var isCameraInit = false.obs;
+
+  initCamera() async{
+    if(await Permission.camera.request().isGranted){
+      cameras = await availableCameras();
+      cameraController = await CameraController(
+          cameras[0],
+          ResolutionPreset.max,
+      );
+      await cameraController.initialize();
+      isCameraInit(true);
+      update();
+    }else{
+      print("Permission Denied");
+    }
+  }
+
+}
+
+
+
